5e48770a93101e724ba3d87bb249e4d8f267a68d
Adithyan <adithyanm.22cse@kongu.edu>
2024-11-10 00:44:11 +0530

    nav

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import logo from "../../assets/logo.png";
+import { IoSearchOutline } from "react-icons/io5";
+
+const Navbar = () => {
+  return (
+    <div className="bg-black h-screen text-white">
+      <div className="flex justify-around items-center">
+        <div className="flex justify-start items-center gap-10">
+          <img src={logo} alt="" className="w-24 md:w-28" />
+          <a href="#">Home</a>
+          <a href="#">TV Shows</a>
+          <a href="#">Movies</a>
+          <a href="#">New & Popular</a>
+          <a href="#">My List</a>
+          <a href="#">Browse by Languages</a>
+        </div>
+        <div className="">
+        <IoSearchOutline />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
