6bca437277a8dd6977093c0a1461c4ea14df0ee8
Adithyan <adithyanm.22cse@kongu.edu>
2024-12-28 22:05:00 +0530

    cards

--- src/Components/Cards_Data/Cards_Data.jsx
+++ src/Components/Cards_Data/Cards_Data.jsx
@@ -1,6 +1,14 @@
 import card1 from "../../assets/kungfupanda.jpg"
 import card2 from "../../assets/squid game.jpg"
 import card3 from "../../assets/squid-challenge.jpg"
+import card4 from "../../assets/jawan.jpg"
+import card5 from "../../assets/the-ghost.jpg"
+import card6 from "../../assets/lucifer.jpg"
+import card7 from "../../assets/the-railway-men.jpg"
+import card8 from "../../assets/young-sheldon.jpg"
+import card9 from "../../assets/sacred-games.jpg"
+import card10 from "../../assets/adipurush.jpg"
+import card11 from "../../assets/sukhee.jpg"
 
 const card_data=[
     {
@@ -14,7 +22,39 @@ const card_data=[
     {
         image:card3,
         name:"Squid Challenge"
-    }
+    },
+    {
+        image:card4,
+        name:"Jawan"
+    },
+    {
+        image:card5,
+        name:"The Ghost"
+    },
+    {
+        image:card6,
+        name:"Lucifer"
+    },
+    {
+        image:card7,
+        name:"The Railway Men"
+    },
+    {
+        image:card8,
+        name:"Young Sheldon"
+    },
+    {
+        image:card9,
+        name:"Sacred Games"
+    },
+    {
+        image:card10,
+        name:"Adipurush"
+    },
+    {
+        image:card11,
+        name:"Sukhee"
+    },
 ]
 
 export default card_data
