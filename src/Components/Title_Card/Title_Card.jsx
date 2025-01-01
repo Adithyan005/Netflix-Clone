96234164e9047dbb2ffd21fcfc653d80c01c5384
Adithyan <adithyanm.22cse@kongu.edu>
2025-01-01 22:52:53 +0530

    card

--- src/Components/Title_Card/Title_Card.jsx
+++ src/Components/Title_Card/Title_Card.jsx
@@ -4,18 +4,13 @@ import card_data from "../Cards_Data/Cards_Data";
 const Title_Card = (props) => {
   return (
     <div className="text-white font-stylish ">
-      <h1 className="text-xl mt-8 font-semibold">
-        {props.title}
-      </h1>
+      <h1 className="text-xl mt-8 font-semibold">{props.title}</h1>
       <div className="ms-4 flex justify-center">
         <div className="scr w-[210vh] overflow-x-scroll mt-4">
           <div className="flex w-[500vh] gap-3">
             {card_data.map((card, index) => {
               return (
-                <div
-                  key={index}
-                  className="text-white"
-                >
+                <div key={index} className="text-white">
                   <img src={card.image} alt="" className="w-[40vw]" />
                   <p className="font-stylish">{card.name}</p>
                 </div>
